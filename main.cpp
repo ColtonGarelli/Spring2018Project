b8f2e442f41396a386b15878d73e0844028c14bf
blockbusterstudio <jeremy.blockbuster@gmail.com>
2017-12-02 18:54:52 -0500

    Templated List and merged Coltons changes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include "TaskLinkedList.h"
 #include "TaskTest.h"
+#include "List.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This is Jeremy's line of text" << std::endl;
-
+    std::cout<<"BReak me"<<std::endl;
     std::cout<<"Colton's text"<<std::endl;
     std::cout<<"show me the formuoli"<<std::endl;
 
     std::cout<<"Taylor's text"<<std::endl;
 
+    List<TaskLinkedList> masterList = List<TaskLinkedList>();
     return 0;
 }
