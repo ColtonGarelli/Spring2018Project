64248e223f2631672696e2b96c9cc9f28cb3f50f
Colton Garelli <coltongarelli@Coltons-MacBook-Pro.local>
2017-12-02 18:25:41 -0500

    latest update

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
     std::cout << "This is Jeremy's line of text" << std::endl;
 
     std::cout<<"Colton's text"<<std::endl;
+    std::cout<<"show me the formuoli"<<std::endl;
 
     std::cout<<"Taylor's text"<<std::endl;
     return 0;
