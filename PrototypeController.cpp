99cb4644b629b0662c33dc377dfac05359a5bff8
Colton Garelli <coltongarelli@Coltons-MacBook-Pro.local>
2017-12-04 09:45:27 -0500

    Updated controller

--- PrototypeController.cpp
+++ PrototypeController.cpp
@@ -6,6 +6,7 @@
 
 
 #include "TaskLinkedList.h"
+#include <iostream>
 
 /*
  *
@@ -15,9 +16,52 @@ int main(){
 
     //must create empty list of tasks, add first task to that
     TaskLinkedList testList1;
-    testList1.addToList()
+    int userDirection=-1;
+    while(userDirection!=3){
+        std::cout<<"Enter 0 to add a new task, 1 to view tasks, 2 to complete tasks, 3 to quit: "<<std::endl;
+        std::cin>>userDirection;
+
+        //add to task
+        if(userDirection==0){
+            Task* newTask= new Task();
+            testList1.addToList(newTask);
+        }
+            //view tasks
+        else if (userDirection==1){
+
+        }
+        //complete a task
+        else if(userDirection==2){
+            //should call view w/ indicies????
+
+            //print tasks and associated numbers
+
+            //for each case do below
+            Task* taskPtr;
+            bool complete =taskPtr->getComplete();
+            if(complete==0){
+                std::cout<<"The task "<<taskPtr->getTitle() <<" is not complete."<<std::endl;
+
+            }
+            else if(complete==1) {
+                std::cout << "The task " << taskPtr->getTitle() << " is complete." << std::endl;
+            }
 
 
+        }
+            //quit program
+        else if(userDirection==3){
+        }
+        else{
+            while(userDirection<0 || userDirection>3){
+                std::cout<<"Invalid entry. Please enter 0 to add task, 1 to view, 2 to complete, 3 to quit"<<std::endl;
+                std::cin>>userDirection;
+            }
+        }
+
+    }
+
+    std::cout<<"\n\nThank you for using the JTC TaskManager."<<std::endl;
 
     //prompts user with request to add a task
 
