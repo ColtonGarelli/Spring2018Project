3c0c316f5ba7a255c39c5395c922dd28848023a9
Colton Garelli <coltongarelli@Coltons-MacBook-Pro.local>
2017-12-03 21:10:12 -0500

    comments on controller

--- PrototypeController.cpp
+++ PrototypeController.cpp
@@ -2,3 +2,31 @@
 // Created by Colton Garelli on 12/3/17.
 //
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ *
+ */
+int main(){
+
+    //must create empty list of tasks, add first task to that
+    //prompts user with request to add a task
+    //prompt user to input field variables (constructor calls set methods to construct new node)
+    //add the node to the list
+    //prompt user to view list, add new task, or complete task
+
+
+
+
+    return 0;
+}
