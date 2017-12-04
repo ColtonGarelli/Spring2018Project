34a91be95ec063f07b3c7358ac29c98ae5943877
Colton Garelli <coltongarelli@Coltons-MacBook-Pro.local>
2017-12-03 21:11:49 -0500

    just thought of something, now i'm done for the night

--- PrototypeController.cpp
+++ PrototypeController.cpp
@@ -23,7 +23,10 @@ int main(){
     //prompts user with request to add a task
     //prompt user to input field variables (constructor calls set methods to construct new node)
     //add the node to the list
-    //prompt user to view list, add new task, or complete task
+    //prompt user to view list, add new task, complete task, quit
+    //do that thing
+
+    //repeat above until quit is entered
 
 
 
